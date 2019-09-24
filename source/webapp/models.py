54ded0775c609384ee4986518ef7a35a016481ba
Айсулу <aisuluueco2009@yandex.ru>
2019-09-24 21:11:29 +0600

    Created models Issue, Status, Type and make migration.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+
+    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name='Summary')
+
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Description')
+
+    status = models.ForeignKey('webapp.Status', related_name='statuses', on_delete=models.PROTECT, verbose_name='Status')
+
+    type = models.ForeignKey('webapp.Type', related_name='types', on_delete=models.PROTECT, verbose_name='Type')
+
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Date of create')
+
+
+    def __str__(self):
+        return self.summary
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.status
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type
