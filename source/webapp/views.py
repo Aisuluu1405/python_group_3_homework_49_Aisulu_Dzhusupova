be738ff7374722b4c242ba9f214c18be7f0682a4
Айсулу <aisuluueco2009@yandex.ru>
2019-09-24 23:42:51 +0600

    Created index page, added files in static folder.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+# from webapp.forms import ArticleForm
+from webapp.models import Issue
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issues'] = Issue.objects.all()
+        return context
