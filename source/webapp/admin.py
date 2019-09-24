0426a443c9437a0a591da00e4786914882a28530
Айсулу <aisuluueco2009@yandex.ru>
2019-09-24 21:48:16 +0600

    Created fixtures and created admin.py and added 5 tasks for examples

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Issue, Status, Type
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type', 'create']
+    list_filter = ['status']
+    search_fields = ['type', 'status']
+    exclude = []
+    readonly_fields = ['create']
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
+
+
