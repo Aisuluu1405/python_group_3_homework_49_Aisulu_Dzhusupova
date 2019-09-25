5e8079d69664e6575a1133ce37dd6b837122d45f
Айсулу <aisuluueco2009@yandex.ru>
2019-09-25 09:39:52 +0600

    Created page 'issue_detail' and modified base.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 
-# from webapp.forms import ArticleForm
+# from webapp.forms import IssueForm
 from webapp.models import Issue
 
 
@@ -12,3 +12,14 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['issues'] = Issue.objects.all()
         return context
+
+
+class IssueView(TemplateView):
+        template_name = 'issue_detail.html'
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            issue_pk = kwargs.get('pk')
+            context['issue'] = get_object_or_404(Issue, pk=issue_pk)
+            return context
+
