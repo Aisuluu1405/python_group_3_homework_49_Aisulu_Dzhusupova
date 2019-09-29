07e41c38cde1710854ecd72e0ff8b449e260ebcf
Айсулу <aisuluueco2009@yandex.ru>
2019-09-29 19:47:23 +0600

    Разделила шаблоны по папкам, немного подправила style.css, внесла корректировки в views.py

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -8,7 +8,8 @@ from django.db.models import ProtectedError
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'issue/index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['issues'] = Issue.objects.all()
@@ -16,49 +17,50 @@ class IndexView(TemplateView):
 
 
 class IssueView(TemplateView):
-        template_name = 'issue_detail.html'
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            issue_pk = kwargs.get('pk')
-            context['issue'] = get_object_or_404(Issue, pk=issue_pk)
-            return context
+    template_name = 'issue/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issue_pk = kwargs.get('pk')
+        context['issue'] = get_object_or_404(Issue, pk=issue_pk)
+        return context
 
 
 class IssueCreateView(View):
+
     def get(self, request, *args, **kwargs):
         form = IssueForm()
-        return render(request, 'issue_add.html', context={'form': form})
+        return render(request, 'issue/add.html', context={'form': form})
+
     def post(self, request, *args, **kwargs):
         form = IssueForm(data=request.POST)
         if form.is_valid():
             issue = Issue.objects.create(
                 summary=form.cleaned_data['summary'],
                 description=form.cleaned_data['description'],
-                create=form.cleaned_data['create'],
                 status=form.cleaned_data['status'],
                 type=form.cleaned_data['type']
             )
             return redirect('detail', pk=issue.pk)
         else:
-            return render(request, 'issue_add.html', context={'form': form})
+            return render(request, 'issue/add.html', context={'form': form})
 
 
-class IssueEditView(TemplateView):
+class IssueEditView(View):
+
     def get(self, request, *args, **kwargs):
-        issue_pk= kwargs.get('pk')
-        issue = get_object_or_404(Issue, pk=issue_pk)
+        issue = get_object_or_404(Issue, pk=kwargs.get('pk'))
         form = IssueForm(data={
             'summary': issue.summary,
             'description': issue.description,
             'status': issue.status_id,
             'type': issue.type_id
-            })
+        })
+        return render(request, 'issue/edit.html', context={'form': form, 'issue': issue})
 
-        return render(request, 'issue_edit.html', context={'form': form, 'issue': issue})
     def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs.get('pk'))
         form = IssueForm(data=request.POST)
-        issue_pk = kwargs.get('pk')
-        issue = get_object_or_404(Issue, pk=issue_pk)
         if form.is_valid():
             data = form.cleaned_data
             issue.summary = data['summary']
@@ -68,15 +70,17 @@ class IssueEditView(TemplateView):
             issue.save()
             return redirect('index')
         else:
-            return render(request, 'issue_edit.html', context={'form': form, 'issue': issue})
+            return render(request, 'issue/edit.html', context={'form': form, 'issue': issue})
 
 
-class IssueDeleteView(TemplateView):
+class IssueDeleteView(View):
+
     def get(self, request, *args, **kwargs):
         issue_pk = kwargs.get('pk')
         issue = get_object_or_404(Issue, pk=issue_pk)
-        return render(request, 'issue_delete.html', context={'issue': issue})
-    def post(self, *args, **kwargs):
+        return render(request, 'issue/delete.html', context={'issue': issue})
+
+    def post(self, request, *args, **kwargs):
         issue_pk = kwargs.get('pk')
         issue = get_object_or_404(Issue, pk=issue_pk)
         issue.delete()
@@ -84,7 +88,8 @@ class IssueDeleteView(TemplateView):
 
 
 class StatusIndexView(TemplateView):
-    template_name = 'status_index.html'
+    template_name = 'status/index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['statuses'] = Status.objects.all()
@@ -92,45 +97,48 @@ class StatusIndexView(TemplateView):
 
 
 class StatusCreateView(View):
+
     def get(self, request, *args, **kwargs):
         form = StatusForm()
-        return render(request, 'status_add.html', context={'form': form})
+        return render(request, 'status/add.html', context={'form': form})
+
     def post(self, request, *args, **kwargs):
         form = StatusForm(data=request.POST)
         if form.is_valid():
             status = Status.objects.create(
-            status=form.cleaned_data['status']
+                status=form.cleaned_data['status']
             )
             return redirect('status_index')
         else:
-            return render(request, 'status_add.html', context={'form': form})
+            return render(request, 'status/add.html', context={'form': form})
 
-class StatusEditView(TemplateView):
+
+class StatusEditView(View):
     def get(self, request, *args, **kwargs):
-        status_pk= kwargs.get('pk')
-        status = get_object_or_404(Status, pk=status_pk)
+        status = get_object_or_404(Status, pk=kwargs.get('pk'))
         form = StatusForm(data={
-            'status': status.status,
-            })
-        return render(request, 'status_edit.html', context={'form': form, 'status': status})
+            'status': status.status
+        })
+        return render(request, 'status/edit.html', context={'form': form, 'status': status})
+
     def post(self, request, *args, **kwargs):
+        status = get_object_or_404(Status, pk=kwargs.get('pk'))
         form = StatusForm(data=request.POST)
-        status_pk = kwargs.get('pk')
-        status = get_object_or_404(Status, pk=status_pk)
         if form.is_valid():
             data = form.cleaned_data
             status.status = data['status']
             status.save()
             return redirect('status_index')
         else:
-            return render(request, 'status_edit.html', context={'form': form, 'status': status})
+            return render(request, 'status/edit.html', context={'form': form, 'status': status})
 
 
-class StatusDeleteView(TemplateView):
+class StatusDeleteView(View):
     def get(self, request, *args, **kwargs):
         status_pk = kwargs.get('pk')
         status = get_object_or_404(Status, pk=status_pk)
-        return render(request, 'status_delete.html', context={'status': status})
+        return render(request, 'status/delete.html', context={'status': status})
+
     def post(self, request, *args, **kwargs):
         status_pk = kwargs.get('pk')
         status = get_object_or_404(Status, pk=status_pk)
@@ -138,11 +146,12 @@ class StatusDeleteView(TemplateView):
             status.delete()
             return redirect('status_index')
         except ProtectedError:
-            return render(request, 'protected_error.html' )
+            return render(request, 'protected_error.html')
 
 
 class TypeIndexView(TemplateView):
-    template_name = 'type_index.html'
+    template_name = 'type/index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['types'] = Type.objects.all()
@@ -150,9 +159,11 @@ class TypeIndexView(TemplateView):
 
 
 class TypeCreateView(View):
+
     def get(self, request, *args, **kwargs):
         form = TypeForm()
-        return render(request, 'type_add.html', context={'form': form})
+        return render(request, 'type/add.html', context={'form': form})
+
     def post(self, request, *args, **kwargs):
         form = TypeForm(data=request.POST)
         if form.is_valid():
@@ -161,17 +172,18 @@ class TypeCreateView(View):
             )
             return redirect('type_index')
         else:
-            return render(request, 'type_add.html', context={'form': form})
+            return render(request, 'type/add.html', context={'form': form})
 
 
-class TypeEditView(TemplateView):
+class TypeEditView(View):
+
     def get(self, request, *args, **kwargs):
         type_pk= kwargs.get('pk')
         type = get_object_or_404(Type, pk=type_pk)
         form = TypeForm(data={
             'type': type.type,
             })
-        return render(request, 'type_edit.html', context={'form': form, 'type':type})
+        return render(request, 'type/edit.html', context={'form': form, 'type':type})
 
     def post(self, request, *args, **kwargs):
         form = TypeForm(data=request.POST)
@@ -183,14 +195,16 @@ class TypeEditView(TemplateView):
             type.save()
             return redirect('type_index')
         else:
-            return render(request, 'type_edit.html', context={'form': form, 'type': type})
+            return render(request, 'type/edit.html', context={'form': form, 'type': type})
 
 
-class TypeDeleteView(TemplateView):
+class TypeDeleteView(View):
+
     def get(self, request, *args, **kwargs):
         type_pk = kwargs.get('pk')
         type = get_object_or_404(Type, pk=type_pk)
-        return render(request, 'type_delete.html', context={'type': type})
+        return render(request, 'type/delete.html', context={'type': type})
+
     def post(self, request, *args, **kwargs):
         type_pk = kwargs.get('pk')
         type = get_object_or_404(Type, pk=type_pk)
