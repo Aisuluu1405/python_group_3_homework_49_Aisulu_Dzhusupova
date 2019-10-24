fd60027a6b329839e52e0222e570f26c5cd50f9e
Айсулу <aisuluueco2009@yandex.ru>
2019-10-24 12:36:23 +0600

    Настроила представление 'login_view' так, чтобы оно перекидывало пользователя на страницу, на которую он хотел зайти, до аутентификации, т.е. по аналогии с LoginRequiredMixin.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
+
 def login_view(request):
     context = {}
+    path_next = request.GET.get('next')
+    path = request.session.setdefault('path', path_next)
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if path:
+                return redirect(path)
             return redirect('webapp:index')
         else:
             context['has_error'] = True
