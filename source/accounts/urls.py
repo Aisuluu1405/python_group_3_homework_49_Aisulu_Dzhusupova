bdfd8b77271f64e3db5689fbf894fcb45ec1a2f4
Айсулу <aisuluueco2009@yandex.ru>
2019-10-27 10:05:27 +0600

    Создала представление для регистрации пользователя, добавила path в 'urls.py'  для регистрации пользователя.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from accounts.views import login_view, logout_view
+from accounts.views import login_view, logout_view, register_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('accounts/login/', login_view, name='login'),
     path('accounts/logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register')
 
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -26,3 +26,17 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:index')
+
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
+
